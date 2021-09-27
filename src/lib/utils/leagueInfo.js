9874132f57ca2aa6f8bc2eca85f4af7e203ab5f9
andrewcal2013 <46458493+andrewcal2013@users.noreply.github.com>
2021-09-27 11:24:27 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Founded in 2020, the league launched as a dynasty league. Each year, the managers compete to win the Crew Cup and avoid the ever-humiliating Toliet Bowl.</p>
+  <p>Founded in 2021, the league launched as a dynasty league. Each year, the managers compete to win the Crew Cup and avoid the ever-humiliating Toliet Bowl.</p>
   <p>Some managers built immediate win-now teams, while others began the slow process of building a juggernaut dynasty team. A portion of the league prioritized youth and draft picks, the rest gravitated towards the steadfast value of veterans. While some managers fled from injuries and down years, others bought the dip and built for the future.</p>
   <p>In astounding commitment to building a dynasty franchise, many of the managers have begun to have kids and groom the next generation of managers to take over their teams. The mini managers are being heavily indoctrinated into the fantasy lifestyle and will be expected to take an active role as soon as they can succesfully type out a coherent trade offer. WHile others are seeking advice on college, job hunting, and general life advice. The goal is to have a positive community of managers to enjoy fantasy football and help each other through life's hurdles.</p>
   <p>In contrast to the addition of the mini managers, some managers have also left but, despite that, the league has only gotten stronger. With more passionate managers, the competition has become more intense and more difficult each year.</p>
